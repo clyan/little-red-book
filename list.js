2b823144f02acf5dafd69f2050a5ff3fb145c6dc
accounts_5e9ef74297567f00378d08e8@mail.teambition.com <accounts_5e9ef74297567f00378d08e8@mail.teambition.com>
2020-05-25 18:08:39 +0800

    更新 list.js

--- list.js
+++ list.js
@@ -2,12 +2,12 @@
 module.exports.handler = function(event, context, callback) {
     try {
       let request = JSON.parse(event);
-      const content = "<h1>Gallery 列表</h1>";
+      let content = "<h1>Gallery 列表</h1>";
       content += "<div>Gallery 列表的 ID 是："+request['pathParameters']['listId']+"</div>";
       content += "<div>它从一个 Restful 的 API中解析得到 request['pathParameters']['listId']</div>";
       content += "<a href='/'>回到首页</a>";
       const response = {
-        isBase64Encoded: false,
+        isBase64Encoded: true,
         statusCode: "200",
         headers: {
           "content-type": "text/html; charset=utf-8"
