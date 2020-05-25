f291ab09a1b645e392dc116dc6d2cd2cbd59e802
accounts_5e9ef74297567f00378d08e8@mail.teambition.com <accounts_5e9ef74297567f00378d08e8@mail.teambition.com>
2020-05-25 17:53:53 +0800

    更新 list.js

---
+++ list.js
@@ -0,0 +1,21 @@
+//ResponseBody 云函数处理入口
+module.exports.handler = function(event, context, callback) {
+    try {
+      let request = JSON.parse(event);
+      const content = "<h1>Gallery 列表</h1>";
+      content += "<div>Gallery 列表的 ID 是："+request['pathParameters']['listId']+"</div>";
+      content += "<div>它从一个 Restful 的 API中解析得到 request['pathParameters']['listId']</div>";
+      content += "<a href='/'>回到首页</a>";
+      const response = {
+        isBase64Encoded: false,
+        statusCode: "200",
+        headers: {
+          "content-type": "text/html; charset=utf-8"
+        },
+        body: new Buffer( content ).toString('base64')
+      };
+      callback(null, response);
+    } catch (err) {
+      callback(err);
+    }
+};
