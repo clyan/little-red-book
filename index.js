713642c1ef1b05e696a73f99c4ac27375e1fcef0
accounts_5e9ef74297567f00378d08e8@mail.teambition.com <accounts_5e9ef74297567f00378d08e8@mail.teambition.com>
2020-05-25 18:09:23 +0800

    更新 index.js

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const uuidv4 = require('uuid/v4');
 //html页面响应，云函数处理入口
 module.exports.handler = function(event, context, callback) {
     try {
-      let request = JSON.parse(event);
-      const strBody = request.isBase64Encoded
-        ? new Buffer(request.body, "base64").toString()
-        : request.body;
-
       const newUUID = uuidv4();
       let bodyContext = "<h1>Hello World</h1><div>您好！ 生成了一个UUID:" + newUUID + "</div>"; 
       bodyContext += "<h1>Hello world</h1><a href='list/200156'>打开 Gallery 列表</a>"
