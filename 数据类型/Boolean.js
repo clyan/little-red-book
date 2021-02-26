10d414148adfb715605b897b597769458de34f80
ywymoshi <46644748+ywymoshi@users.noreply.github.com>
2021-02-27 01:09:41 +0800

    'Boolean.js'

--- 数据类型/Boolean.js
+++ 数据类型/Boolean.js
@@ -8,3 +8,12 @@ log(Boolean(undefined))
 log(Boolean(false))
 log(Boolean(0))
 log(Boolean(NaN))
+
+// 包装成对象，进行布尔运算时也是按照对象的转换规则
+let booleanObject = new Boolean(false);
+log(booleanObject) // [Boolean: false]
+log(booleanObject && true) // true
+log(false && true) // false
+log(typeof booleanObject) // object
+log(typeof false) // boolean
+
